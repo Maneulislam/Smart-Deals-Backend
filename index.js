42dd4896a5c9fd560da5fc95ec6d9beb0a393d9f
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2026-05-11 15:43:52 +0600

    final deploy 6

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const admin = require("firebase-admin");
 var jwt = require('jsonwebtoken');
+const admin = require("firebase-admin");
 
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        }),
+        // ... any other config
+    });
+} else {
+    admin.app(); // use the already initialized app
+}
 
 const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8");
 const serviceAccount = JSON.parse(decoded);
